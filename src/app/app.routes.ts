acc00c60dc38d55bbe8b24e3ef193c2aa398db5c
Nanthakumar <nanthanaveen565@gmail.com>
2025-08-23 17:35:49 +0530

    [header and nav]

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './compnents/about/about.component';
+import { ContactComponent } from './compnents/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'home',
+        component:HomeComponent
+    },
+    {
+        path:'about',
+        component:AboutComponent
+    },
+    {
+        path:'contact',
+        component:ContactComponent
+    },
+];
