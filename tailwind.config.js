ea420359c71120155250f129948034cadb7e5838
Nanthakumar <nanthanaveen565@gmail.com>
2026-02-11 17:11:49 +0530

    [change img]

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}",
-
-  ],
+  darkMode: 'class', // 👈 REQUIRED
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
+
 
 
 
